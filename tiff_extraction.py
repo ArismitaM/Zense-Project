4cca9034cc6b45efbefbbd496689580139014bcb
Arismita Mukherjee <146760434+ArismitaM@users.noreply.github.com>
2024-08-01 08:19:10 +0530

    initial tiff_extraction.py

--- tiff_extraction.py
+++ tiff_extraction.py
@@ -1,3 +1,4 @@
+import numpy as np
 import rasterio
 from rasterio.plot import show
 from matplotlib import pyplot as plt
@@ -5,18 +6,12 @@ from matplotlib import pyplot as plt
 img = rasterio.open('/home/arismita/ML/landCover/data/archive/images/train/tokyo_37.tif')
 #img = rasterio.open('/home/arismita/ML/landCover/data/archive/label/train/tokyo_37.tif')
 #img = rasterio.open('/home/arismita/ML/landCover/data/archive/label/train/aachen_63.tif')
+#img = rasterio.open('/home/arismita/ML/landCover/data/archive/images/train/tokyo_37.tif')
+img = rasterio.open('/home/arismita/ML/landCover/data/archive/label/train/tokyo_37.tif')
 
 print ("The opened file details: ", img)
 print ("The read metadata: ", img.meta)
-print ("The read shape: ", img.shape)
-#data = img.read()
-#print("The read data: ", data)
-show(img)
-# X and Y are supposed to be latitude and longitude if u have the right metadata 
-
-full_img = img.read() #Note the three bands and the shape of image
-
-# To find the no. of bands in an image
+@@ -20,22 +20,17 @@
 num_bands = img.count
 print("Number of bands in the image = ", num_bands)
 
@@ -25,6 +20,13 @@ for i in range (1, num_bands + 1):
     #print ("The read metadata1: ", img_band1.meta)
     show(img_band)
     print (img_band)
+img_band = img.read(1) #stands for the 1st band
+#print ("The read metadata1: ", img_band1.meta)
+show(img_band)
+print(img_band.shape)
+print(img.colorinterp[0])
+#for i in range (1, 101):
+ #   print (img_band[i])
 
 '''
 fig = plt.figure(figsize=(10,10))
@@ -38,3 +40,9 @@ print("BEFORE AX3")
 ax3 = fig.add_subplot(2,2,3)
 ax3.imshow(img_band3,cmap='pink')
 '''
+arr = np.array(img_band)
+unique_elements = np.unique(arr) 
+print(unique_elements) 
+
+print(list(zip(*np.where(img_band == 5))))
+#print(output)
